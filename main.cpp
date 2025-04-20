09b2476c15c4ee50708b700c99678a4d0b9f8aa3
Bilal079 <149462836+Bilal079@users.noreply.github.com>
2025-04-21 02:36:53 +0500

    updated main

--- main.cpp
+++ main.cpp
@@ -1,43 +1,67 @@
-#include "chunk.h"
-#include "crypto.h"
 #include <iostream>
-#include <fstream>
+#include <string>
+#include <vector>
+#include "master.h"
+#include "worker.h"
+#include "crypto.h"
 using namespace std;
 
-int main() {
-    string inputFilePath = "input.txt"; //Input file to be encrypted
-    string encryptedFilePath = "encrypted_output.txt"; //Encrypted output
-    size_t chunkSize = 1024 * 1024; //1 MB chunks
+void runWorker(const string& address) {
+    EncryptionWorker worker;
+    worker.runServer(address);
+}
 
-    try {
-        //Generate AES key and IV
-        string key, iv;
-        AESCrypto::generateKeyIV(key, iv);
-
-        //Chunk the file
-        vector<FileChunk> chunks = FileChunker::chunkFile(inputFilePath, chunkSize);
-
-        //Encrypt each chunk
-        vector<FileChunk> encryptedChunks;
-        for (const auto& chunk : chunks) {
-            auto encryptedData = AESCrypto::encrypt(chunk.data, key, iv);
-
-            FileChunk encryptedChunk;
-            encryptedChunk.id = chunk.id;
-            encryptedChunk.data.assign(encryptedData.begin(), encryptedData.end());
-            encryptedChunks.push_back(encryptedChunk);
-        }
-
-        //Reassemble encrypted chunks
-        if (FileChunker::reassembleFile(encryptedFilePath, encryptedChunks)) {
-            cout << "Encryption complete. Output written to " << encryptedFilePath << endl;
-        } else {
-            cerr << "Failed to write encrypted file.\n";
-        }
-
-    } catch (const exception& ex) {
-        cerr << "Error: " << ex.what() << endl;
+void runMaster(const vector<string>& workerAddresses, 
+              const string& inputFile, 
+              const string& outputFile) {
+    EncryptionMaster master(workerAddresses);
+    
+    //Generate key and IV
+    string key, iv;
+    AESCrypto::generateKeyIV(key, iv);
+    
+    cout << "Using key: " << key << endl;
+    cout << "Using IV: " << iv << endl;
+    
+    //Encrypt the file
+    const size_t chunkSize = 1024 * 1024; //1MB chunks
+    auto encryptedChunks = master.encryptFile(inputFile, chunkSize, key, iv);
+    
+    //Reassemble the file
+    if (FileChunker::reassembleFile(outputFile, encryptedChunks)) {
+        cout << "File encrypted successfully: " << outputFile << endl;
+    } else {
+        cerr << "Failed to reassemble encrypted file" << endl;
     }
+}
 
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <mode> [options]" << endl;
+        cerr << "Modes:" << endl;
+        cerr << "  worker <address> - Run as worker node" << endl;
+        cerr << "  master <input> <output> <worker1> [worker2...] - Run as master node" << endl;
+        return 1;
+    }
+    
+    string mode(argv[1]);
+    
+    if (mode == "worker" && argc >= 3) {
+        runWorker(argv[2]);
+    } else if (mode == "master" && argc >= 5) {
+        string inputFile(argv[2]);
+        string outputFile(argv[3]);
+        vector<string> workerAddresses;
+        
+        for (int i = 4; i < argc; ++i) {
+            workerAddresses.push_back(argv[i]);
+        }
+        
+        runMaster(workerAddresses, inputFile, outputFile);
+    } else {
+        cerr << "Invalid arguments" << endl;
+        return 1;
+    }
+    
     return 0;
 }
